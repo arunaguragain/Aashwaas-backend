6ecf96dba131c0fda513a7b6a0b1de0ec59dc947
arunaguragain <guragainaruna@gmail.com>
2026-01-02 20:44:11 +0545

    added auth route and database in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express , {Application, Request, Response} from 'express';
 import bodyParser from 'body-parser';
+import { connectDatabase } from './database/mongodb';
 import { PORT } from './config';
+import authRoutes from "./routes/auth.route";
 
 const app: Application = express();
 
@@ -10,9 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ success: "true", message: "Welcome to the api of aashwaas" });
 }); 
+app.use('/api/auth', authRoutes);
 
 async function startServer() {
-    // await connectDatabase();
+    await connectDatabase();
 
     app.listen(
         PORT,
