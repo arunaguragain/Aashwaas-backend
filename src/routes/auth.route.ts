f5f8f85832aa96452be1927bc59b5404b010c24e
arunaguragain <guragainaruna@gmail.com>
2026-01-29 21:09:50 +0545

    added admin routed and update image routes in auth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizedMiddleware } from "../middlewares/authorization.middleware";
+import { uploads } from "../middlewares/upload.middleware";
 
 let authController = new AuthController();
 const router = Router();
 
 router.post("/register", authController.register)
 router.post("/login", authController.login)
+router.put("/:id", authorizedMiddleware, uploads.single("image"), authController.updateProfile)
 
 export default router;
