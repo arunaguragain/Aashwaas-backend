a7f957877088773d9adc450e2967290352881fd9
arunaguragain <guragainaruna@gmail.com>
2026-02-06 10:43:34 +0545

    segregatted the app.ts and index.ts for testing purpose

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express , {Application, Request, Response} from 'express';
+import bodyParser from 'body-parser';
+import { connectDatabase } from './database/mongodb';
+import { PORT } from './config';
+import authRoutes from "./routes/auth.route";
+import adminUserRoutes from "./routes/admin/user.route";
+import donationRoutes from "./routes/donation.route";
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+console.log(process.env.PORT); 
+
+const app: Application = express();
+
+let corsOptions={
+    origin:["http://localhost:3000","http://localhost:3003"],
+    //which url can access backend
+    //put your frontend domain/url here
+}
+//origin:"*",//yo le sabai url lai access dincha
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from uploads directory
+app.use('/item_photos', express.static(path.join(__dirname, '../uploads')));
+
+app.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({ success: "true", message: "Welcome to the api of aashwaas" });
+}); 
+app.use('/api/auth', authRoutes);
+app.use('/api/donations', donationRoutes);
+app.use('/api/admin/users', adminUserRoutes);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,6 @@
-import express , {Application, Request, Response} from 'express';
-import bodyParser from 'body-parser';
-import { connectDatabase } from './database/mongodb';
-import { PORT } from './config';
-import authRoutes from "./routes/auth.route";
-import adminUserRoutes from "./routes/admin/user.route";
-import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
-console.log(process.env.PORT); 
-
-const app: Application = express();
-
-let corsOptions={
-    origin:["http://localhost:3000","http://localhost:3003"],
-    //which url can access backend
-    //put your frontend domain/url here
-}
-//origin:"*",//yo le sabai url lai access dincha
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req: Request, res: Response) => {
-    return res.status(200).json({ success: "true", message: "Welcome to the api of aashwaas" });
-}); 
-app.use('/api/auth', authRoutes);
-app.use('/api/admin/users', adminUserRoutes);
+import app from "./app";
+import { PORT } from "./config";
+import { connectDatabase } from "./database/mongodb";
 
 async function start() {
     await connectDatabase();
